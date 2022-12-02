6fe06efe969b61c52cec05df5bf49f3ed1b5c0b8
Nick Wu <nickwu@indeed.com>
2022-12-01 23:19:54 -0600

    update https://coolors.co/palette/da2c38-226f54-87c38f-f4f0bb-43291f

--- pmlxem.markdown
+++
@@ -1,7 +0,0 @@
-pmLxeM
-------
-
-
-A [Pen](https://codepen.io/Silvia_Chen/pen/pmLxeM) by [Katness C.](https://codepen.io/Silvia_Chen) on [CodePen](https://codepen.io).
-
-[License](https://codepen.io/license/pen/pmLxeM).
--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const questions = [
+        {
+          text: "畫畫接力",
+          icon: "fas fa-video"
+        },
+        {
+          text: "一年級表演",
+          icon: "fas fa-birthday-cake"
+        },
+        {
+          text: "二年級表演",
+          icon: "fas fa-star"
+        },
+        {
+          text: "三年級表演",
+          icon: "fas fa-baby"
+        },
+        {
+          text: "猜12生肖裡的動物",
+          icon: "fas fa-plane"
+        },
+        {
+          text: "猜是誰是家長 ",
+          icon: "fas fa-bicycle"
+        },
+        {
+          text: "比手畫腳",
+          icon: "fas fa-wifi"
+        },
+        {
+          text: "恐佈箱",
+          icon: "fab fa-buromobelexperte"
+        },
+        {
+          text: "上台支援前線 ",
+          icon: "fas fa-certificate"
+        },
+        {
+          text: "機智問題",
+          icon: "fas fa-user-graduate"
+        },
+      ]
+
 let vm = new Vue({
   el: '#app',
   computed: {
@@ -23,56 +66,7 @@ let vm = new Vue({
   },
   data() {
     return {
-      prizes: [
-        {
-          text: "Movie",
-          icon: "fas fa-video"
-        },
-        {
-          text: "Wish",
-          icon: "fas fa-birthday-cake"
-        },
-        {
-          text: "Star",
-          icon: "fas fa-star"
-        },
-        {
-          text: "Child",
-          icon: "fas fa-baby"
-        },
-        {
-          text: "Flight",
-          icon: "fas fa-plane"
-        },
-        {
-          text: "Bike",
-          icon: "fas fa-bicycle"
-        },
-        {
-          text: "Wifi",
-          icon: "fas fa-wifi"
-        },
-        {
-          text: "Toy",
-          icon: "fab fa-buromobelexperte"
-        },
-        {
-          text: "Award",
-          icon: "fas fa-certificate"
-        },
-        {
-          text: "Dog",
-          icon: "fas fa-paw"
-        },
-        {
-          text: "Award",
-          icon: "fas fa-certificate"
-        },
-        {
-          text: "Dog",
-          icon: "fas fa-paw"
-        }
-      ],
+      prizes: questions,
       r: 0,
       isShowResult: false
     };
@@ -87,6 +81,36 @@ let vm = new Vue({
     turningEnd() {
       this.$refs.roulette.classList.remove("turning");
       this.isShowResult = true;
+      document.getElementById(`question-${this.awardIdx}`).innerHTML = questions[this.awardIdx].text;
     }
   }
 })
+
+const teams = [
+  {
+    text: "識字班",
+    icon: "fas fa-fish"
+  },
+  {
+    text: "注音班",
+    icon: "fas fa-cat"
+  },
+  {
+    text: "一年級",
+    icon: "fas fa-dove"
+  },
+]
+
+window.onload = function() {
+  document.getElementById("questions").innerHTML = questions
+    .map((q, i) => {
+      return `<tr><td class="${q.icon}"></td><td class="text" id="question-${i}"></td></tr>`;
+    })
+    .join("");
+
+  document.getElementById("teams").innerHTML = teams
+    .map((team, i) => {
+      return `<tr><td class="${team.icon} text" id="team-${i}">${team.text}</td><td>1000</td></tr>`;
+    })
+    .join("");
+}
